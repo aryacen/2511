efd1e0aec76fbdb59a8ff99e301ec452c49bdda6
aryacen <aryabodhi.gunawan@student.unsw.edu.au>
2022-07-13 01:50:57 +1000

    finalized implementaion of newGame

---
+++ src/test/java/dungeonmania/NewGameTests.java
@@ -0,0 +1,131 @@
+package dungeonmania;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static dungeonmania.TestUtils.getPlayer;
+import static dungeonmania.TestUtils.getEntities;
+import static dungeonmania.TestUtils.getInventory;
+import static dungeonmania.TestUtils.getGoals;
+import static dungeonmania.TestUtils.countEntityOfType;
+import static dungeonmania.TestUtils.getValueFromConfigFile;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.DungeonManiaController;
+import dungeonmania.response.models.BattleResponse;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.response.models.RoundResponse;
+import dungeonmania.response.models.ItemResponse;
+import dungeonmania.util.FileLoader;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+public class NewGameTests {
+
+    @Test
+    public void testNewGameBasic() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungeonRes = dmc.newGame("zombies", "simple");
+        Position position1 = new Position(1, 1);
+        Position position2 = new Position(1, 2);
+        Position position3 = new Position(2, 1);
+        Position position4 = new Position(8, 8);
+        EntityResponse entity1 = new EntityResponse("player1", "player", position1, false);
+        EntityResponse entity2 = new EntityResponse("sword1", "sword", position2, false);
+        EntityResponse entity3 = new EntityResponse("zombie_toast_spawner1", "zombie_toast_spawner", position3, true);
+        EntityResponse entity4 = new EntityResponse("exit1", "exit", position4, false);
+        List<EntityResponse> entities = new ArrayList<EntityResponse>();
+        List<ItemResponse> inventory = new ArrayList<ItemResponse>();
+        List<BattleResponse> battles = new ArrayList<BattleResponse>();
+        List<String> buildables = new ArrayList<String>();
+        entities.add(entity1);
+        entities.add(entity2);
+        entities.add(entity3);
+        entities.add(entity4);
+        String goal = ":exit";
+        DungeonResponse expectedOutput = new DungeonResponse("zombies1", "zombies", entities, inventory, battles,
+                buildables, goal);
+        assertEquals(expectedOutput.getDungeonId(), initDungeonRes.getDungeonId());
+        assertEquals(expectedOutput.getDungeonName(), initDungeonRes.getDungeonName());
+        assertTrue(expectedOutput.getEntities().equals(initDungeonRes.getEntities()));
+        assertTrue(expectedOutput.getInventory().equals(initDungeonRes.getInventory()));
+        assertTrue(expectedOutput.getBattles().equals(initDungeonRes.getBattles()));
+        assertTrue(expectedOutput.getBuildables().equals(initDungeonRes.getBuildables()));
+        assertEquals(expectedOutput.getGoals(), initDungeonRes.getGoals());
+    }
+
+    @Test
+    public void testNewGameBuildBow() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungeonRes = dmc.newGame("build_bow", "simple");
+        Position position1 = new Position(1, 1);
+        Position position2 = new Position(2, 1);
+        Position position3 = new Position(3, 1);
+        Position position4 = new Position(4, 1);
+        Position position5 = new Position(5, 1);
+        Position position6 = new Position(0, 2);
+        EntityResponse entity1 = new EntityResponse("player1", "player", position1, false);
+        EntityResponse entity2 = new EntityResponse("wood1", "wood", position2, false);
+        EntityResponse entity3 = new EntityResponse("arrow1", "arrow", position3, false);
+        EntityResponse entity4 = new EntityResponse("arrow2", "arrow", position4, false);
+        EntityResponse entity5 = new EntityResponse("arrow3", "arrow", position5, false);
+        EntityResponse entity6 = new EntityResponse("exit1", "exit", position6, false);
+        List<EntityResponse> entities = new ArrayList<EntityResponse>();
+        List<ItemResponse> inventory = new ArrayList<ItemResponse>();
+        List<BattleResponse> battles = new ArrayList<BattleResponse>();
+        List<String> buildables = new ArrayList<String>();
+        entities.add(entity1);
+        entities.add(entity2);
+        entities.add(entity3);
+        entities.add(entity4);
+        entities.add(entity5);
+        entities.add(entity6);
+        String goal = ":exit";
+        DungeonResponse expectedOutput = new DungeonResponse("build_bow1", "build_bow", entities, inventory, battles,
+                buildables, goal);
+        assertEquals(expectedOutput.getDungeonId(), initDungeonRes.getDungeonId());
+        assertEquals(expectedOutput.getDungeonName(), initDungeonRes.getDungeonName());
+        assertTrue(expectedOutput.getEntities().equals(initDungeonRes.getEntities()));
+        assertTrue(expectedOutput.getInventory().equals(initDungeonRes.getInventory()));
+        assertTrue(expectedOutput.getBattles().equals(initDungeonRes.getBattles()));
+        assertTrue(expectedOutput.getBuildables().equals(initDungeonRes.getBuildables()));
+        assertEquals(expectedOutput.getGoals(), initDungeonRes.getGoals());
+
+    }
+
+    @Test
+    public void testNewGameIdSameDungeon() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        dmc.newGame("zombies", "simple");
+        DungeonResponse initDungeonRes = dmc.newGame("zombies", "no_spider_spawning");
+        String expectedDungeonId = "zombies2";
+        assertEquals(expectedDungeonId, initDungeonRes.getDungeonId());
+
+    }
+
+    @Test
+    public void testNewGameGoals() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungeonRes = dmc.newGame("advanced", "simple");
+        String expectedGoal = ":enemies AND :treasure";
+        assertEquals(expectedGoal, initDungeonRes.getGoals());
+
+        DungeonResponse initDungeonRes1 = dmc.newGame("exit_goal_order", "simple");
+        String expectedGoal1 = ":boulders AND (:exit AND :treasure)";
+        assertEquals(expectedGoal1, initDungeonRes1.getGoals());
+
+        DungeonResponse initDungeonRes2 = dmc.newGame("zombies", "simple");
+        String expectedGoal2 = ":exit";
+        assertEquals(expectedGoal2, initDungeonRes2.getGoals());
+    }
+}
