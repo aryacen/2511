fcce051e619cf60f04acf4a9e505377387d04ac0
Roy <roy.lim@student.unsw.edu.au>
2022-07-13 17:28:20 +1000

    completed adding and removing items from Inventory

--- src/main/java/dungeonmania/Entities/Item/Inventory.java
+++ src/main/java/dungeonmania/Entities/Item/Inventory.java
@@ -1,5 +1,6 @@
 package dungeonmania.Entities.Item;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -9,7 +10,8 @@ This class contains the player's inventory
 public class Inventory {
     // Each item is stored in a list that will keep track of how many items there are
     // Each item list is stored as a string list pair in a hash map
-    HashMap<String, ArrayList> items;
+    private HashMap<String, ArrayList<Item>> items;
+    public static int noSuchItem = -1;
 
     public Inventory() {
         items = new HashMap<>();
@@ -21,34 +23,53 @@ public class Inventory {
      * @return -1 if item has not found, otherwise returns how many instances of the item the player has
      */
     public int hasItem(Item i) {
-        return -1;
+        if (!this.items.containsKey(i.getEntityName())) {
+            return items.get(i.getEntityName()).size();
+        }
+        else {
+            return noSuchItem;
+        }
     }
 
     /**
      * Adds an item into the players inventory
      */
-    public void addItem() {
-
+    public void addItem(Item i) {
+        // If there are no items, created a new list
+        if (hasItem(i) == noSuchItem) {
+            this.items.put(i.getEntityName(), new ArrayList<>());
+        }
+        // Add the item to the list
+        this.items.get(i.getEntityName()).add(i);
     }
 
     /**
      * Removes one instance of the item
      * @Pre-condition Must have at least one instance of the item in the inventory
      */
-    public void removeItem() {
-        // Access the item list using the hashmap
-        // Remove the last item
-        // If the list is empty, remove it from the hashmap
+    public void removeItem(String itemName) {
+        /*
+         Access the item list using the hashmap
+         Remove the last item
+        */
+        ArrayList<Item> itemList = this.items.get(itemName);
+        itemList.remove(itemList.size() - 1);
+
+        /* If the list is empty, remove it from the hashmap */
+        if (itemList.isEmpty()) {
+            items.remove(itemName);
+        }
+
     }
 
     /**
      * Removes n instances of the item
      * @Pre-condition Must have at least n instances of the item in the inventory
      */
-    public void removeItem(int n) {
-        // Access the item list using the hashmap
-        // Remove the n last item
-        // If the list is empty, remove it from the hashmap
+    public void removeItem(String itemName, int n) {
+        for (int i = 0; i < n; i++) {
+            removeItem(itemName);
+        }
     }
 
     /**
@@ -58,5 +79,6 @@ public class Inventory {
     public void decreaseDurability() {
         // Decrease the durability of an item
         // If durability reaches 0, remove the item by calling removeItem()
+        // TODO: FINISH THIS
     }
 }
