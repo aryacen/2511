3d6d07513c32ab25475a9d17e83740897a874ad6
Roy <roy.lim@student.unsw.edu.au>
2022-07-13 18:01:19 +1000

    added toDo for buildable items

--- src/main/java/dungeonmania/Entities/Item/BuildableEntities/BuildableEntity.java
+++ src/main/java/dungeonmania/Entities/Item/BuildableEntities/BuildableEntity.java
@@ -2,7 +2,12 @@ package dungeonmania.Entities.Item.BuildableEntities;
 
 import dungeonmania.Entities.Item.Item;
 
+import java.util.HashMap;
+
 public abstract class BuildableEntity extends Item {
+    // Each buildable entity has a recipe that needs to be met
+    // TODO: FIGURE HOW HOW TO CRAFT THE ITEMS
+    public static HashMap<String, Integer> recipe;
     public BuildableEntity(String id, String type) {
         super(id, type);
     }
