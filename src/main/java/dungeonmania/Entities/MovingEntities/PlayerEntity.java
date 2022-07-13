ba75f21ef3345b3b3de122caa1345a758bde7a0b
Roy <roy.lim@student.unsw.edu.au>
2022-07-13 21:04:09 +1000

    fixed moving entites

--- src/main/java/dungeonmania/Entities/MovingEntities/PlayerEntity.java
+++ src/main/java/dungeonmania/Entities/MovingEntities/PlayerEntity.java
@@ -3,17 +3,19 @@ package dungeonmania.Entities.MovingEntities;
 import dungeonmania.Entities.Entity;
 import dungeonmania.Entities.Item.CraftingSystem;
 import dungeonmania.Entities.Item.Inventory;
+import dungeonmania.util.Position;
 
 public class PlayerEntity extends Entity{
 
     private CraftingSystem c;
     private Inventory i;
 
-    public PlayerEntity() {
-        super("Player", "MovingEntities");
+    public PlayerEntity(String id, String type, Position position, boolean isInteractable) {
+        super(id, type, position, false);
         c = new CraftingSystem();
         i = new Inventory();
     }
+
 
     // Can bribe the mercenary
     
