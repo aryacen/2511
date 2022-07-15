877c291e74327ed2ab0b65145b86218c8afc0405
Roy Lim <roy.lim@student.unsw.edu.au>
2022-07-15 21:26:20 +1000

    Revert "movement tests"
    This reverts commit ec0a35fe6fc13c7d185a8cb1c0a4bc0c5dcae5d2

--- src/test/java/dungeonmania/ItemTests.java
+++ src/test/java/dungeonmania/ItemTests.java
@@ -3,29 +3,6 @@ package dungeonmania;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import static dungeonmania.TestUtils.getPlayer;
-import static dungeonmania.TestUtils.getEntities;
-import static dungeonmania.TestUtils.getInventory;
-import static dungeonmania.TestUtils.getGoals;
-import static dungeonmania.TestUtils.countEntityOfType;
-import static dungeonmania.TestUtils.getValueFromConfigFile;
-import dungeonmania.exceptions.InvalidActionException;
-
-
-
-import dungeonmania.response.models.BattleResponse;
-import dungeonmania.response.models.DungeonResponse;
-import dungeonmania.response.models.EntityResponse;
-import dungeonmania.response.models.RoundResponse;
-import dungeonmania.util.Direction;
-import dungeonmania.util.Position;
 /*
 This test is for the usage of any item
  */
@@ -36,17 +13,7 @@ public class ItemTests {
     @Test
     @DisplayName("Test player can pick up item")
     public void testPlayerPickUpItem() {
-        DungeonManiaController dmc;
-        dmc = new DungeonManiaController();
-        DungeonResponse res = dmc.newGame("d_itemTest_testPlayerPickUpItem.json",
-                "c_simple");
-        res = dmc.tick(Direction.RIGHT);
-        assertEquals(1, getInventory(res, "sword").size());
-        res = dmc.tick(Direction.RIGHT);
-        assertEquals(1, getInventory(res, "wood").size());
-        res = dmc.tick(Direction.RIGHT);
-        assertEquals(2, getInventory(res, "wood").size());
-        
+
     }
     /*
     Movement with Doors and Keys
