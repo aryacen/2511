ba152fa86b058961fac3c3dc45894e73f05d1907
Roy Lim <roy.lim@student.unsw.edu.au>
2022-07-31 17:28:38 +1000

    Roy persistence

--- src/main/java/dungeonmania/Entities/MovingEntities/PlayerEntity.java
+++ src/main/java/dungeonmania/Entities/MovingEntities/PlayerEntity.java
@@ -47,12 +47,12 @@ public class PlayerEntity extends MovingEntity {
         this.i = new Inventory();
 
         // Uncomment when persistence is done
-        // if (j.has("inventory")) {
-        // this.i = new Inventory((JSONArray) j.get("inventory"));
-        // }
-        // else {
-        // this.i = new Inventory();
-        // }
+//        if (j.has("inventory")) {
+//            this.i = new Inventory((JSONArray) j.get("inventory"));
+//        }
+//        else {
+//            this.i = new Inventory();
+//        }
     }
 
     /**
@@ -115,34 +115,33 @@ public class PlayerEntity extends MovingEntity {
     }
 
     public void interact(String entityId, ArrayList<StaticEntity> staticEntities,
-            ArrayList<MovingEntity> movingEntities) throws IllegalArgumentException, InvalidActionException {
+             ArrayList<MovingEntity> movingEntities) throws IllegalArgumentException, InvalidActionException {
 
         Entity interactable = null;
         ZombieToastSpawnerEntity spawner = null;
         MercenaryEntity merc = null;
         /*
-         * interactable = movingEntities.stream().filter(me ->
-         * me.getId().equals(entityId)).findFirst();
-         * if (interactable == null) {
-         * interactable = staticEntities.stream().filter(se ->
-         * se.getId().equals(entityId)).findFirst();
-         * }
-         */
-        for (MovingEntity entity : movingEntities) {
-            if (entity.getId().equals(entityId)) {
-                switch (entity.getType()) {
+        interactable = movingEntities.stream().filter(me -> me.getId().equals(entityId)).findFirst();
+        if (interactable == null) {
+            interactable = staticEntities.stream().filter(se -> se.getId().equals(entityId)).findFirst();
+        }
+        */
+        for (MovingEntity entity: movingEntities) {
+            if(entity.getId().equals(entityId)) {
+                switch(entity.getType()) {
                     case "mercenary":
                         merc = (MercenaryEntity) entity;
                         break;
                     case "assassin":
                         break;
-
+                    
+                        
                 }
                 interactable = entity;
-
+                
             }
         }
-
+             
         if (interactable == null) {
             for (StaticEntity entity : staticEntities) {
                 if (entity.getId().equals(entityId)) {
@@ -151,7 +150,7 @@ public class PlayerEntity extends MovingEntity {
                 }
             }
         }
-
+        
         if (interactable == null || !interactable.isInteractable()) {
             throw new IllegalArgumentException("Object is not interactable");
         } else {
@@ -167,10 +166,10 @@ public class PlayerEntity extends MovingEntity {
                     // this.interacter.bribeMercenary(interactable, this, i, staticEntities);
                     break;
                 case "assassin":
-
+                     
             }
         }
-
+        
     }
     // Can bribe the mercenary
 
@@ -207,11 +206,12 @@ public class PlayerEntity extends MovingEntity {
     }
 
     // Uncomment when persistence is done
-    // @Override
-    // public JSONObject getJSON() {
-    // JSONObject j = super.getJSON();
-    // j.put("inventory", this.i.getJSON());
-    // return j;
-    // }
+//    @Override
+//    public JSONObject getJSON() {
+//        JSONObject j = super.getJSON();
+//        j.put("inventory", this.i.getJSON());
+//        return j;
+//    }
+
 
 }
